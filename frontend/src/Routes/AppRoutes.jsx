507cd5ff5bf699e5a4e6f81d846c239eaac2c62a
Ankit Kumar <ankitkumar021303@gmail.com>
2025-01-02 01:29:38 +0530

    Configured the Axios setup.

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 // import React from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
+// Import Screens
 import Login from '../screens/Login';
 import Register from '../screens/Register';
+import Home from '../screens/Home';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
